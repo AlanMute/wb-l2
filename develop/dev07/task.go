9c9d42f5e39bab8b18415c3bbb753893350b921a
Krizzis <alanasia904@gmail.com>
2023-12-07 21:33:14 +0300

    Done dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -7,32 +13,46 @@ package main
 Одним из вариантов было бы очевидно написать выражение при помощи select, которое бы реализовывало эту связь,
 однако иногда неизестно общее число done каналов, с которыми вы работаете в рантайме.
 В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or каналов, реализовывала весь функционал.
-
-Определение функции:
-var or func(channels ...<- chan interface{}) <- chan interface{}
-
-Пример использования функции:
-sig := func(after time.Duration) <- chan interface{} {
-	c := make(chan interface{})
-	go func() {
-		defer close(c)
-		time.Sleep(after)
-}()
-return c
-}
-
-start := time.Now()
-<-or (
-	sig(2*time.Hour),
-	sig(5*time.Minute),
-	sig(1*time.Second),
-	sig(1*time.Hour),
-	sig(1*time.Minute),
-)
-
-fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func or(ch ...<-chan interface{}) <-chan interface{} {
+	wg := sync.WaitGroup{}
+	single := make(chan interface{})
+	defer close(single)
 
+	for _, c := range ch {
+		wg.Add(1)
+		go func(cc <-chan interface{}) {
+			for v := range cc {
+				single <- v
+			}
+			wg.Done()
+		}(c)
+	}
+
+	wg.Wait()
+
+	return single
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("fone after %v", time.Since(start))
 }
