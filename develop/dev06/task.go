01bfa4cf8cc6ef0ef7101ba9980120c22c9c6bc4
Krizzis <alanasia904@gmail.com>
2023-12-07 17:10:13 +0300

    Done dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -14,5 +24,74 @@ package main
 */
 
 func main() {
+	fields := flag.Int("f", 1, "выбрать поля (колонки)")
+	delimiter := flag.String("d", "\t", "использовать другой разделитель")
+	separated := flag.Bool("s", false, "только строки с разделителем")
 
+	flag.Parse()
+
+	if *fields < 1 {
+		*fields = 1
+	}
+
+	filename := flag.Arg(0)
+	if filename == "" {
+		log.Fatal("Введите название файла!")
+	}
+
+	data := ReadFile(filename)
+	fmt.Printf("Результат:\n\n")
+
+	res := cut(data, *fields, *delimiter, *separated)
+	for _, s := range res {
+		fmt.Println(s)
+	}
+}
+
+func cut(data []string, fields int, delimiter string, separated bool) []string {
+	var res []string
+
+	for _, s := range data {
+		t := strings.Split(s, delimiter)
+		if separated {
+			if t[0] != s {
+				if fields < len(t) {
+					res = append(res, t[fields-1])
+				}
+			}
+		} else {
+			if fields < len(t) {
+				res = append(res, t[fields-1])
+			}
+		}
+	}
+
+	return res
+}
+
+func ReadFile(filename string) []string {
+
+	file, err := os.Open(filename)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("Ошибка открытия файла", filename)
+	}
+
+	sc := bufio.NewReader(file)
+	var rows []string
+
+	for {
+		l, err := sc.ReadString('\n')
+		if err != nil && err != io.EOF {
+			log.Fatal(err.Error())
+		}
+		l = strings.TrimRight(l, "\r\n")
+		rows = append(rows, l)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return rows
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert"
+)
+
+func Test_cut(t *testing.T) {
+	var table = []struct {
+		input     []string
+		fields    int
+		delimiter string
+		separated bool
+		expectOut []string
+	}{
+		{
+			input: []string{
+				"a b c",
+				"d e f",
+				"g h i",
+			},
+			fields:    1,
+			delimiter: " ",
+			separated: false,
+			expectOut: []string{
+				"a", "d", "g",
+			},
+		},
+		{
+			input: []string{
+				"a b c",
+				"d e f",
+				"g h i",
+			},
+			fields:    2,
+			delimiter: " ",
+			separated: false,
+			expectOut: []string{
+				"b", "e", "h",
+			},
+		},
+		{
+			input: []string{
+				"a b c",
+				"d e f",
+				"g h i",
+			},
+			fields:    4,
+			delimiter: " ",
+			separated: false,
+		},
+		{
+			input: []string{
+				"a b c",
+				"d e f",
+				"ghi",
+			},
+			fields:    1,
+			delimiter: " ",
+			separated: true,
+			expectOut: []string{
+				"a", "d",
+			},
+		},
+	}
+
+	for _, test := range table {
+		output := cut(test.input, test.fields, test.delimiter, test.separated)
+
+		assert.Equal(t, output, test.expectOut)
+	}
+}
