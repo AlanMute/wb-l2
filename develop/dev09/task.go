9093ad0ca09fe76164e486d2ecd3f7e47772fda5
Krizzis <alanasia904@gmail.com>
2023-12-08 21:09:47 +0300

    Done dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,13 +1,54 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита wget ===
-
 Реализовать утилиту wget с возможностью скачивать сайты целиком
-
-Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
 func main() {
+	flag.Parse()
+	site := flag.Arg(0)
+	if len(site) == 0 {
+		log.Fatal("Введите url сайта!")
+	}
+	Wget(site)
+	fmt.Println("Скачивание прошло успешно!")
+}
 
+func GetPathFile(s string) string {
+	sr := strings.Split(s, "/")[2] + ".html"
+	return sr
+}
+
+func Wget(s string) {
+	resp, err := http.Get(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	path := GetPathFile(s)
+
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
